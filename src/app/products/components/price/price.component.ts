10fbc473847a25ffcfc96a3a4d82ff621322f2c9
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2024-01-05 14:43:06 +0100

    fin de ciclos de vida

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.scss']
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  ngOnInit(): void {
+    console.log('Price (Hijo) component: ngOnInit');
 
+    this.interval$ = interval(1000).subscribe(value => console.log(`Tick: ${value}`));
+
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Price (Hijo) component: ngOnChanges');
+    console.log({ changes });
+  }
+  ngOnDestroy(): void {
+    this.interval$?.unsubscribe();
+  }
+
+  @Input()
+  public price: number = 0;
+
+  public interval$?: Subscription;
 }
